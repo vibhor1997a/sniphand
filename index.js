8bac1b06b1a6239451bd4d1e1bd65d3e03ad099e
vibhor1997a <vibhor349a@rediffmail.com>
2017-11-30 11:04:53 +0530

    better ui

--- index.js
+++ index.js
@@ -1,34 +1,41 @@
-#! /usr/bin/env node
-const readline = require('readline');
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-let bodyPath = process.argv[2];
-let obj = process.argv[3];
-let fs = require('fs');
-let json = require('C://Users//pc//AppData//Roaming//Code//User//snippets//javascript.json', 'utf-8');
-if (fs.existsSync(bodyPath)) {
-    let body = fs.readFileSync(bodyPath, 'utf-8');
-    body = body.split('\n');
-    if (json[obj]) {
-        json[obj].body = body;
-    }
-    else {
-        json[obj] = {};
-        json[obj].prefix = json[obj].description = obj;
-        json[obj].body = body;
-    }
-    let file = JSON.stringify(json, null, "\t");
-    fs.writeFile('C://Users//pc//AppData//Roaming//Code//User//snippets//javascript.json', file, (err) => {
-        if (!err) {
-            console.log("Success!");
+#! /usr/bin / env node
+let co = require('co');
+let prompt = require('co-prompt');
+co(function* () {
+    let name = yield prompt('Name for the snippet: ');
+    let prefix = yield prompt('prefix for the snippet: ');
+    let bodyPath = yield prompt('path for the file containing body:\n');
+    let description = yield prompt('description for the snippet: ');
+    let fs = require('fs');
+    let json = require('C://Users//pc//AppData//Roaming//Code//User//snippets//javascript.json', 'utf-8');
+    if (fs.existsSync(bodyPath)) {
+        let body = fs.readFileSync(bodyPath, 'utf-8');
+        body = body.split('\n');
+        if (json[name]) {
+            json[name].prefix = prefix;
+            json[name].body = body;
+            json[name].description = description;
         }
         else {
-            console.log('Error Writing file!');
+            json[name] = {};
+            json[name].prefix = prefix
+            json[name].description = description;
+            json[name].body = body;
         }
-    });
-}
-else {
-    console.log("Invalid Input!");
-}
+        let file = JSON.stringify(json, null, "\t");
+        fs.writeFile('C://Users//pc//AppData//Roaming//Code//User//snippets//javascript.json', file, (err) => {
+            if (!err) {
+                console.log("Success!");
+                process.exit(0);
+            }
+            else {
+                console.log('Error Writing file!');
+                process.exit(1);
+            }
+        });
+    }
+    else {
+        console.log("Invalid Input!");
+        process.exit(1);
+    }
+});
