15c69417a29ee253037f866bce74b53624dc8fe7
vibhor1997a <vibhor349a@rediffmail.com>
2017-11-30 12:28:18 +0530

    colors

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ co(function* () {
         let file = JSON.stringify(json, null, "\t");
         fs.writeFile('C://Users//pc//AppData//Roaming//Code//User//snippets//javascript.json', file, (err) => {
             if (!err) {
-                console.log(chalk.greenBright("Success!"));
+                console.log(chalk.green("Success!"));
                 process.exit(0);
             }
             else {
